262cceb9a7bf504dccad504f201065be7f75726c
jano <jano@HI18621.cegekanv.corp.local>
2012-02-24 14:11:35 +0100

    test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,8 +4,8 @@ import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static void index() {
-        render();
-    }
+	public static void index() {
+		render();
+	}
 
 }
