263fd414f37daf56cc4ab9cca17b31575f0a8d28
Tim Schraepen <sch3lp@gmail.com>
2012-02-24 16:46:59 +0100

    redirect to login, instead of GET

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,14 +5,16 @@ import play.mvc.Controller;
 
 public class Application extends Controller {
 
-    public static final String CEGEKA_SOCIALTEXT_ROOT_PATH = "http://cegeka.socialtext.net";
-
     public static void index() {
 		render();
 	}
 
 	public static void signals() {
 		render();
+	}
+
+	public static void login() {
+		signals();
 	}
 
 }
