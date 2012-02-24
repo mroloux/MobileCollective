467ebe461bf390049ecf19e0de3c9cae5e25f3a0
Tim Schraepen <sch3lp@gmail.com>
2012-02-24 16:31:38 +0100

    security "enabled" (visible in url)

--- app/controllers/SocialText.java
+++ app/controllers/SocialText.java
@@ -9,23 +9,25 @@ import play.mvc.Controller;
 
 public class SocialText extends Controller {
 
-	public static void signals() {
-		String result = createSocialTextRequest("http://cegeka.socialtext.net/data/signals")
+    private static final String HTTP_CEGEKA_SOCIALTEXT_NET = "http://cegeka.socialtext.net";
+
+    public static void signals(String user, String pwd) {
+		String result = createSocialTextRequest(HTTP_CEGEKA_SOCIALTEXT_NET + "/data/signals", user, pwd)
 				.setHeader("Accept", "application/json")
 				.get()
 				.getString();
 		renderJSON(result);
 	}
 
-	public static void photo(String userId) throws IOException {
-		InputStream result = createSocialTextRequest("http://cegeka.socialtext.net/data/people/" + userId + "/photo")
+	public static void photo(String userId,String user, String pwd) throws IOException {
+		InputStream result = createSocialTextRequest(HTTP_CEGEKA_SOCIALTEXT_NET + "/data/people/" + userId + "/photo", user, pwd)
 				.get()
 				.getStream();
 		renderBinary(result);
 	}
 
-	private static WSRequest createSocialTextRequest(String url) {
-		return WS.url(url).authenticate("matti.roloux@cegeka.be", "");
+	private static WSRequest createSocialTextRequest(String url, String user, String pwd) {
+		return WS.url(url).authenticate(user, pwd);
 	}
 
 }
